bdca473db86842ab950a6bddf792420aaa1f7ea6
Pedro Ilustre <pedro.ilustre.web@gmail.com>
2023-03-23 16:16:57 -0300

    feat: finish sidebar

--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react';
 import styles from './Sidebar.module.css';
 
 export function Sidebar(){
@@ -8,11 +9,18 @@ export function Sidebar(){
                 src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=407&q=50" 
             />
             <div className={styles.profile}>
+                <img 
+                    src="https://media.licdn.com/dms/image/C4D03AQEvjEtvnbiiPw/profile-displayphoto-shrink_800_800/0/1661982949041?e=1684972800&v=beta&t=OH-FEOgDMjETCH2H-LVawAgE4omfajghgcLFFLqnqfY" 
+                    className={styles.avatar} 
+                />
                 <strong>Pedro Ilustre</strong>
                 <span> Software Developer</span>
             </div>
             <footer>
-                <a href="#">Editar seu perfil</a>
+                <a href="#"> 
+                    <PencilLine size={20} /> 
+                    Editar seu perfil
+                </a>
             </footer>
         </aside>
     );
