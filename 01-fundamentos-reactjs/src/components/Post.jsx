eb7304db847e6dbcf322aa167352019cbcc649b1
Pedro Ilustre <pedro.ilustre.web@gmail.com>
2023-03-31 22:52:39 -0300

    feat: comment form done

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -21,8 +21,18 @@ export function Post(){
                     <a href="">#nlw</a> {' '}
                     <a href="">#novprojeto</a> {' '}
                     <a href="">#rocketseat</a> {' '}
-                     </p>
+                </p>
             </div>
+
+            <form className={styles.commentForm}>
+                <strong> Deixe seu feedback</strong>
+                <textarea 
+                    placeholder='deixe seu comment'
+                />
+                <footer>
+                    <button type="submit">Publicar</button>
+                </footer>
+            </form>
         </article>
     );
 }
