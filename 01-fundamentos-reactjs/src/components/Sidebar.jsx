a1b0e4135d025b65bb6e01cc2c2bee6252373360
Pedro Ilustre <pedro.ilustre.web@gmail.com>
2023-03-31 22:03:24 -0300

    fix: change class from sidebard to sidebar

--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import styles from './Sidebar.module.css';
 
 export function Sidebar(){
     return (
-        <aside className={styles.sidebard}>
+        <aside className={styles.sidebar}>
             <img 
                 className={styles.cover}
                 src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=407&q=50" 
